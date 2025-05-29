37e25cae231e0b72f9648cf145ef5d79e6e2b0ce
Bhavaniswary <128512880+Bhavaniswary@users.noreply.github.com>
2025-05-29 11:32:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function login() {
+  const user = document.getElementById("username").value;
+  const pass = document.getElementById("password").value;
+
+  // Basic check (you can add real checks later)
+  if (user === "admin" && pass === "1234") {
+    window.location.href = "home.html";
+    return false; // Prevent form submission
+  } else {
+    alert("Invalid credentials!");
+    return false;
+  }
+}
